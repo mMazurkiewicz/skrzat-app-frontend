59ce5647bb9fe810494e2ecd15848dbe82374c02
Mateusz Mazurkiewicz <mazur@Mazur.local>
2020-06-19 13:54:28 +0200

    venues sorting and filtering

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 
 const theme = createMuiTheme({
   palette: {
@@ -15,10 +15,10 @@ const theme = createMuiTheme({
 });
 
 ReactDOM.render(
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>,
-    document.getElementById('root')
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>,
+  document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
